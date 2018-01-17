32c596daa41044884ba01bbafa9e2d1bbe3575e6
Josué Mercadillo <josuemercally@gmail.com>
2018-01-17 11:37:18 -0600

    Initial commit 6
    Se versionan carpetas Web>Security, Custom

---
+++ Web/Web.Custom/DataNotation/PlaceHolderAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.ModelBinding;
+
+namespace Web.Custom.DataNotation
+{
+    public class PlaceHolderAttribute : Attribute, IMetadataAware
+    {
+        private readonly string _placeholder;
+        public PlaceHolderAttribute(string placeholder)
+        {
+            _placeholder = placeholder;
+        }
+
+        public void OnMetadataCreated(ModelMetadata metadata)
+        {
+            metadata.AdditionalValues["placeholder"] = _placeholder;
+        }
+    }
+}
